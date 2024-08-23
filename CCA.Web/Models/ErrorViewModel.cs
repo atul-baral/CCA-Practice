698cf28bbec7018bd2c5a8f213544b30254694bb
Atul Baral <atul.baral8421@gmail.com>
2024-08-23 15:45:49 +0545

    Initial commit

---
+++ CCA.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CCA.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
